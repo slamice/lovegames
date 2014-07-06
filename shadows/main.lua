decac9dfe704d9b9a5c5de9958aeaac6a0b9abb7
slamice <slamice@gmail.com>
2014-07-07 00:54:59 +0900

    added (stole from tut) simple graphics

--- shadows/main.lua
+++ shadows/main.lua
@@ -1,3 +1,75 @@
+function love.load()
+    g = love.graphics
+    playerColor = {255,0,128}
+    groundColor = {25,200,25}
+    
+    -- instantiate our player and set initial values
+    require "Player"
+    p = Player:new()
+    
+    p.x = 300
+    p.y = 300
+    p.width = 25
+    p.height = 40
+    p.jumpSpeed = -800
+    p.runSpeed = 500
+    
+    gravity = 1800
+    
+    yFloor = 500
+end
+ 
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        p:moveRight()
+    end
+    if love.keyboard.isDown("left") then
+        p:moveLeft()
+    end
+    
+    -- if the x key is pressed...
+    if love.keyboard.isDown("x") then
+    -- make the player jump
+        p:jump()
+    end
+ 
+    -- update the player's position
+    p:update(dt, gravity)
+ 
+    -- stop the player when they hit the borders
+    if p.x > 800 - p.width then p.x = 800 - p.width end
+    if p.x < 0 then p.x = 0 end
+    if p.y < 0 then p.y = 0 end
+    if p.y > yFloor - p.height then
+        p:hitFloor(yFloor)
+    end
+end
+ 
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+    -- round down our x, y values
+    local x = math.floor(p.x)
+    local y = math.floor(p.y)
+    
+    -- draw the player shape
+    g.setColor(playerColor)
+    g.rectangle("fill", x, y, p.width, p.height)
+    
+    -- draw the ground
+    g.setColor(groundColor)
+    g.rectangle("fill", 0, yFloor, 800, 100)
+    
+    -- debug information
+    g.setColor(255, 255, 255)
+    local isTrue = ""
+    g.print("Player coordinates: ("..x..","..y..")", 5, 5)
+    g.print("Current state: "..p.state, 5, 20)
+end
+ 
+function love.keyreleased(key)
+    if key == "escape" then
+        love.event.push("q")   -- actually causes the app to quit
+    end
+    if (key == "right") or (key == "left") then
+        p:stop()
+    end
 end
