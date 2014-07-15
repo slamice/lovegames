4fcb7390cf4432c2e71cbc4daf1dbc3479a8a49c
slamice <slamice@gmail.com>
2014-07-15 23:37:21 +0900

    added collider functionality again

--- shadows/main.lua
+++ shadows/main.lua
@@ -1,49 +1,77 @@
 require "Player"
 require "/lib/helpers"
+HC = require '/lib/HardonCollider'
 g = love.graphics
 f = love.filesystem
 path = "/Users/slamice/Documents/development/Lua/lovegames/shadows/"
 assets_path = ''
 
+-- array to hold collision messages
+local text = {}
+
+-- this is called when two shapes collide
+function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
+    text[#text+1] = string.format("Colliding. mtv = (%s,%s)", 
+                                    mtv_x, mtv_y)
+end
+
+-- this is called when two shapes stop colliding
+function collision_stop(dt, shape_a, shape_b)
+    text[#text+1] = "Stopped colliding"
+end
+
 -- Initial Load function
 function love.load()
-    img = { hero = love.graphics.newImage("hero.png")}
+    h = Helpers
+    Collider = HC(100, on_collision, collision_stop)
+
+    img = { hero = g.newImage("hero.png")}
     pos = { 100 ,100 } -- these are the (random at game start) x/y coordinates of the unit
     speed = 200
     moveDir = { 0, 0 } -- movement direction: {-1,-1} would be 100% left and 100% up, {0,1} would be 100% right. We will add ways to change moveDir later.
-    love.graphics.setBackgroundColor(255,255,255)
+    g.setBackgroundColor(255,255,255)
+
+    local lines = lines_from(path.."level1.txt")
+
+
+    circle = Collider:addCircle(pos[1],pos[2],20)
+
 end
  
 function love.update(dt)
+  pos = {pos[1] + (moveDir[1] * speed * dt), 
+         pos[2] + (moveDir[2] * speed * dt)}
 
-   pos = {pos[1] + (moveDir[1] * speed * dt), pos[2] + (moveDir[2] * speed * dt)}
+  -- check for collisions
+  -- Collider:update(dt)
+
 end
 
 function love.draw(dt)
---[[
-    local lines = lines_from(path.."level1.txt")
 
-    -- print all line numbers and their contents
-    for k,v in pairs(lines) do
-      
-        block = h:split(v,',')
+  -- Player
+  g.draw(img.hero, pos[1], pos[2])
+  g.print("Player coordinates: x:"..pos[1]..", y:"..pos[2], 5, 25)
+  
+   -- print all line numbers and their contents
+  for i in pairs(lines) do
+    
+      block = h:split(lines[i],',')
 
-        -- draw the ground
-        posx = block[1]
-        posy = block[2]
-        groundx = block[3]
-        groundy = block[4]
+      -- draw the ground
+      posx = block[1]
+      posy = block[2]
+      groundx = block[3]
+      groundy = block[4]
 
-        -- shapes can be drawn to the screen
-        g.setColor(groundColor)
-        rect = collider:addRectangle(200,400,groundx,groundy)
-        rect:draw('fill')
-       -- g.rectangle(posx, posy, groundx, groundy)
-        g.print("Floor coordinates: x:"..groundx..", y:"..groundy, 5, 35)
-    end
-]]
+      -- shapes can be drawn to the screen
+      -- rect = g.rectangle("fill",posx,posy,groundx,groundy)
+     rect = Collider:addRectangle(100,100,100,100)
+     rect:draw('fill')
+     g.setColor(0,0,0)
+  end
 
-   love.graphics.draw(img.hero, pos[1], pos[2])
+   g.print(text)
 end
 
 function love.keypressed(key, unicode)
