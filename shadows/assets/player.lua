88830d7932614b4f17cc01039d1ee429f5b201b6
slamice <slamice@gmail.com>
2014-08-02 12:08:27 +0900

    made jumping less crappy

--- shadows/assets/player.lua
+++ shadows/assets/player.lua
@@ -12,8 +12,9 @@ function player:initialize(newWorld, l,t,w,h)
   self.img = g.newImage("/assets/hero.png")
   world = newWorld 
   world:add(self,l,t,w,h)
+  self.onGround = false
   self.speed = 200
-  self.gravity = 9.81
+  self.gravity = 1
   self.canJump = true
   self.fall_velocity = 100
   --self.map = map
@@ -27,15 +28,22 @@ function player:update(dt)
     dx = self.speed * dt
   elseif love.keyboard.isDown('left') then
     dx = -self.speed * dt
-  elseif love.keyboard.isDown('up') then
-    dy = dy - self.speed * dt * self.gravity
+  end
+  if love.keyboard.isDown('up') and self.onGround == true then
+    dy = dy - self.speed * dt * 12
+  end
+
+  cols, len = world:check(player)
+
+  if len == 0 then
+    self.onGround = false
   end
 
 --[[  if love.keyboard.isDown('down') then
     dy = self.speed * dt
 ]]
 
-  dy = dy + (self.gravity*self.fall_velocity*dt)
+  dy = dy + (self.gravity * self.fall_velocity * dt)
 
   if dx ~= 0 or dy ~= 0 then
     local future_l, future_t = player.l + dx, player.t + dy
@@ -46,6 +54,7 @@ function player:update(dt)
     else
       local col, tl, tt, sl, st
       while len > 0 do
+        self.onGround = true
         col = cols[1]
         tl,tt,_,_,sl,st = col:getSlide()
         player.l, player.t = tl, tt
@@ -58,6 +67,12 @@ function player:update(dt)
       end
     end
   end
+end
+
+-- Return the number of collisions
+function player:getCols()
+  local collisions, len = world:check(B)
+  assert(len == 0)
 end
 
 function player:draw()
